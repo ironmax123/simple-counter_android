40a192c3c9cf6c220666c1b42f4bad53c007ebd9
eita <>
2024-11-01 14:30:03 +0900

    update

--- app/src/main/java/com/example/counter_app/MainActivity.kt
+++ app/src/main/java/com/example/counter_app/MainActivity.kt
@@ -14,7 +14,7 @@ class MainActivity : AppCompatActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge() // システムバーに合わせた表示を調整
+        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
         
